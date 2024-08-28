ce28161f5f2bff6f51ea72a7e2e28516953715e1
leandroposleman96 <leandroposleman@hotmail.com>
2024-08-28 18:00:46 +0000

    auto commit 3       20240828 180033

---
+++ src/rpart/225_nuevas_semillas.r
@@ -0,0 +1,16 @@
+# como generar muchas semillas a partir de una
+
+require("primes")
+
+# genero numeros primos
+primos <- generate_primes(min = 100000, max = 1000000)
+
+
+set.seed(100049) # inicializo 
+
+# me quedo con por ejemplo 20 primos al azar
+semillas <- sample(primos, 20 )
+
+print( semillas )
+
+
